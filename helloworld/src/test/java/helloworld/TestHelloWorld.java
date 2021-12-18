8e1174acc923b6cb5a0af97ba2fe6be2d0b74f5d
Jacqueline <jacky2502@me.com>
2021-12-18 16:30:04 +0100

    neues Reposirory erstellt

---
+++ helloworld/src/test/java/helloworld/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld()  {
+	
+	System.out.println("..Hallo Welt, mein Name ist Jacky!");
+	
+	}
+	
+}
